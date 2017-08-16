f463cdfa6fcddf1a1814cf2e83724662ad01f72e
Emilie Thoreson <thoresonemilie@gmail.com>
2017-08-16 10:45:16 -0700

    add edit and order buttons, add takeOneDown() logic

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,22 +6,54 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Emerald City Smoothies</h1>
     <h2>Live Menu</h2>
-    <ul>
-      <li *ngFor="let smoothie of smoothies">Name: {{smoothie.name}}
-      <br>Price: {{smoothie.price}}
-      <br>Ingredients: {{smoothie.ingredients}}
-      <br>Pints Left: {{smoothie.pints}}</li>
-    </ul>
+    <table>
+      <tr ng-style="{'text-align' : left}">
+        <th>Name:</th>
+        <th>Price:</th>
+        <th>Ingredients:</th>
+        <th>Pints Left:</th>
+        <th>Edit:</th>
+        <th>Order 1:</th>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies">{{smoothie.name}}</td>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies">{{smoothie.price}}</td>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies">{{smoothie.ingredients}}</td>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies">{{smoothie.pints}}</td>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies"><button (click)="editSmoothie()">Edit!</button></td>
+      </tr>
+      <tr>
+        <td *ngFor="let smoothie of smoothies"><button (click)="takeOneDown(smoothie)">Place Order</button></td>
+      </tr>
+    </table>
   </div>
   `
 })
 
 export class AppComponent {
   smoothies: Smoothie[] = [
-    new Smoothie('Orange you glad', 6, ['orange', 'banana']),
-    new Smoothie('Banana-rama', 5, ['banana', 'strawberry', 'mango']),
+    new Smoothie('Orange you glad', 6, ['orange']),
+    new Smoothie('Banana-rama', 5, ['banana', 'strawberry']),
     new Smoothie('Mango-agogo', 4, ['mango', 'pineapple'])
   ];
+
+
+  takeOneDown(clickedSmoothie: Smoothie) {
+    clickedSmoothie.pints -= 1;
+    alert(clickedSmoothie.pints);
+  }
+
+  editSmoothie() {
+    alert("you just edited a smoothie!");
+  }
 }
 
 export class Smoothie {
